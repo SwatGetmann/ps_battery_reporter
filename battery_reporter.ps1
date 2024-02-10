3c4b7e56449ba3e9946445bcaa40891b22436c85
SwatGetmann <swatgetmann@gmail.com>
2024-02-10 17:14:38 +0400

    powershell script for taking battery reports

---
+++ battery_reporter.ps1
@@ -0,0 +1,13 @@
+$dt = Get-Date -Format "yyMMdd"
+
+$char = if (Test-Path ".\$dt.log")
+{
+    $last_char = Get-Content -Path ".\$dt.log" -Tail 1
+    [char](([int][char]$last_char)++)
+}
+else
+{
+    "a"
+}
+
+powercfg.exe /batteryreport /output "battery-report-$dt-$char.html"
