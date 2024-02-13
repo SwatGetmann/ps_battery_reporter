abdc45c674db9144c087720b0dc7f037da53f2f8
SwatGetmann <swatgetmann@gmail.com>
2024-02-13 09:05:04 +0400

    add dir as param

--- battery_reporter.ps1
+++ battery_reporter.ps1
@@ -1,17 +1,24 @@
+param(
+    [string]$dir = "./"
+)
+
+if (!(Test-Path -PathType Container $dir)) {
+    New-Item -ItemType Directory -Path $dir
+}
+
 $dt = Get-Date -Format "yyMMdd"
 
-$char = if (Test-Path ".\$dt.log")
-{
-    $last_char = Get-Content -Path ".\$dt.log" -Tail 1
+$log_path = "$dir/$dt.log"
+
+$char = if (Test-Path $log_path) {
+    $last_char = Get-Content -Path $log_path -Tail 1
     $last_char = ([int][char]$last_char)
     $last_char = $last_char + 1
     [char]$last_char
-}
-else
-{
+} else {
     "a"
 }
 
-powercfg.exe /batteryreport /output "battery-report-$dt-$char.html"
+powercfg.exe /batteryreport /output "$dir/battery-report-$dt-$char.html"
 
-Add-Content ".\$dt.log" $char
+Add-Content $log_path $char
